11c0c2ee7337e11d30ef5308894f7a331c29483c
melaniam <melania.miron@fortech.ro>
2016-06-08 18:11:13 +0300

    added fixtures

---
+++ tests/fixtures/product.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+	sku: '0123456789',
+	title: 'Choco & Caramel minis',
+	category: 'Food',
+	details: {
+		description: '20 individually wrapped chocolate and caramel bars in different flavours',
+		summary: 'Chocolate and caramel mini bars'
+	},
+	price: {
+		currency: 'RON',
+		factor: 100,
+		amount: 599
+	},
+	active: true
+}
---
+++ tests/fixtures/user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  _id: '5731eb718dc033bc69d9660f',
+  email: 'register_user@test.com',
+  name: 'New Test User',
+  password: '09a3fcee233197960eb1bafe82e20021ad2807e8b12c79e9822ba0d5addfdcb7f786a365b2e8de0419f24cf671f2bd8bac9e6aacd5ff5bece1e5e7346fc19c1741e0286659e39577aaa4a6fd4fe0eab30b95d4437f3821838e27f311bda42a70ae47656eea65afd65a856c9079c866d591df466538cfe7abff4578f3d93fb9e6',
+  passwordSalt: 'a6a465b8c1baf79bb9423c249ef8a523bd7e34600b3b2215562870a35895fdab',
+  active: true,
+  createdAt: 'Tue May 10 2016 17:08:49 GMT+0300 (GTB Daylight Time)',
+};
